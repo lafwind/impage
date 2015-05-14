d27a7d4e409ac308b0c7ecc8f8cf3d54b11afee5
Lafwind <lafwind@gmail.com>
2015-05-14 21:40:07 +0800

    完成用户注册登入等功能

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+
   has_attached_file :image, styles:  { large: "800x600>", medium: "400x300>", small: "200x150>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
