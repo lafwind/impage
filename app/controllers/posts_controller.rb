7a9d81daef5709423baeaf8b47a3e2ca954bdf6d
Lafwind <lafwind@gmail.com>
2015-05-14 20:53:41 +0800

    setup paperclip for image uploading and modify .gitignore file for paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,7 +46,7 @@ class PostsController < ApplicationController
     end
 
     def post_params
-      params.require(:post).permit(:title, :description)
+      params.require(:post).permit(:title, :description, :image)
     end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+  has_attached_file :image, styles:  { large: "800x600>", medium: "400x300>", small: "200x150>" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
