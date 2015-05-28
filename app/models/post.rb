1e86de0f55e2f892548d5289549a95365440a819
Lafwind <lafwind@gmail.com>
2015-05-28 15:56:47 +0800

    完成show页面设计

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
 
-  has_attached_file :image, styles:  { large: "800x600>", medium: "400x300>", small: "200x150>" }
+  has_attached_file :image, styles:  { large: "600x400>", medium: "400x300>", small: "200x150>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
