04a255b54d703ac8d2aa377033e62ed7ec8dcb18
Lafwind <lafwind@gmail.com>
2015-06-01 14:58:31 +0800

    修复bug: 让paperclip的aws配置只在生产环境中生效

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,5 @@
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:hash.:extension'
-Paperclip::Attachment.default_options[:hash_secret] = 'get_use_rake_secret'
+if Rails.env.production?
+  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:hash.:extension'
+  Paperclip::Attachment.default_options[:hash_secret] = 'get_use_rake_secret'
+end
