5dfc3cf5900b5cd7e37a1aa84df48cd99b045ba3
Lafwind <lafwind@gmail.com>
2015-06-02 17:10:52 +0800

    完成显示收藏(赞过的)功能

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,4 +8,12 @@ class StaticPagesController < ApplicationController
       @posts = Post.where(user_id: current_user).order("created_at DESC").paginate(page: params[:page])
     end
   end
+
+  def like_list
+    if params[:tag]
+      @posts = Post.where(user_id: current_user).tagged_with(params[:tag]).order("created_at DESC").paginate(page: params[:page])
+    else
+      @posts = Post.where(user_id: current_user).order("created_at DESC").paginate(page: params[:page])
+    end
+  end
 end
