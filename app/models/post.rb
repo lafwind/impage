caaf5c3458056c836524d1d462682d612a4aebb5
Lafwind <lafwind@gmail.com>
2015-11-17 01:29:31 +0800

    修改 posts#show 页面

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
 
-  has_attached_file :image, styles:  { large: "2400x1600>", medium: "600x400>", small: "200x150>" }
+  has_attached_file :image, styles:  { large: "2400x1600#", medium: "600x400>", small: "200x150>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   self.per_page = 10
